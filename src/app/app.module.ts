848381253ba4e7d11a5208a7b8c02d65c6ffa39c
Ida Farhan <idaslack46@gmail.com>
2022-05-15 14:12:35 -0400

    Initial settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RealestateComponent } from './realestate/realestate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RealestateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/realestate/realestate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-realestate',
+  templateUrl: './realestate.component.html',
+  styleUrls: ['./realestate.component.scss']
+})
+export class RealestateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

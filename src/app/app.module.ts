45f998e63325c68f6d07aaf455600f038ac95839
Ida Farhan <idaslack46@gmail.com>
2022-05-26 11:47:56 -0400

    Adding sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RealestateComponent } from './realestate/realestate.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RealestateComponent
+    RealestateComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/realestate/realestate.component.ts
+++ src/app/realestate/realestate.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import * as Data from '../input.json';  
 
 
+interface realestate {
+  price: string,
+  image: string,
+  title: string,
+  url: string,
+  bd: string,
+  ba: string,
+  ft: string,
+  location: string,
+  house: string
+}
 
 @Component({
   selector: 'app-realestate',
@@ -9,10 +20,14 @@ import * as Data from '../input.json';
   styleUrls: ['./realestate.component.scss']
 })
 export class RealestateComponent implements OnInit {
-
+  realEstates: any = Data;
+  showFiller = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.realEstates = JSON.parse(JSON.stringify(this.realEstates));
+    console.log(this.realEstates)
   }
 
 }
